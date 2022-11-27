1732fc3d8a4760b56958bc100c042ef25e132984
jumpert <juan.perezc@correo.ucu.edu.uy>
2022-11-27 18:35:19 -0300

    Merge branch 'main' of https://github.com/jumpert/ProyectoFinal

--- ProyectoFinal/src/app/shopping-cart/shopping-cart.component.ts
+++ ProyectoFinal/src/app/shopping-cart/shopping-cart.component.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { CartItem } from '../models/cart-item';
+import { User } from '../models/user';
 import { CartItemService } from '../services/cart-item.service';
 import { ShoppingCartService } from '../services/shopping-cart.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -10,20 +12,24 @@ import { ShoppingCartService } from '../services/shopping-cart.service';
 })
 export class ShoppingCartComponent implements OnInit {
   public cartItems: CartItem[] = [];
+  public user?: User;
+  public ownCart?: CartItem[]=[];
   @Output() toggleCartVisibility = new EventEmitter<void>();
   title: string = "Carrito";
-  endMsg:string = "Finalizar Compra";
+  endMsg: string = "Finalizar Compra";
 
-  constructor(public cartItemService: CartItemService, public shoppingCartService: ShoppingCartService) {}
+  constructor(public cartItemService: CartItemService, public shoppingCartService: ShoppingCartService, public uS: UserService) { }
   
   ngOnInit(): void {
     this.getItems();
+    this.getUser();
   }
+
   showItems = true;
   onCartClicked(): void {
     this.toggleCartVisibility.emit();
   }
-  
+
   get total(): number {
 
     return this.cartItems.reduce((acc, { price }) => (acc += price), 0);
@@ -39,17 +45,22 @@ export class ShoppingCartComponent implements OnInit {
   addItem(item: CartItem): void {
     this.cartItems = [...this.cartItems, item];
   }
-  
+
   getItems():void {
     this.cartItemService.getItems()
       .subscribe(Items => this.cartItems = Items);
   }
- 
+
   toggleItemVisibility():void {
     this.showItems = !this.showItems;
-  }  
+  }
 
   checkout(): void {
     this.cartItems = [];
+  }
+
+  getUser(): void {
+    this.uS.getUser(1)
+      .subscribe(user => this.user = user);
   }
 }
