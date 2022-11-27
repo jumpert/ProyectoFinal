4c82df334836117fab965aaf6679eeac30dfb541
jumpert <juan.perezc@correo.ucu.edu.uy>
2022-11-26 22:36:25 -0300

    user, nav-tool, product

--- ProyectoFinal/src/app/services/shopping-cart.service.ts
+++ ProyectoFinal/src/app/services/shopping-cart.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CartItem } from '../models/cart-item';
 
@@ -5,14 +6,25 @@ import { CartItem } from '../models/cart-item';
   providedIn: 'root'
 })
 export class ShoppingCartService {
+  private url = 'http://localhost:3000/api/v1/shoppingCart';
   
-  
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  constructor(private http: HttpClient) { }
   cartItems: CartItem[] = []
 
   get total(): number {
     return this.cartItems.reduce((acc, { price }) => (acc += price), 0);
   }
+  /*deleteItem(itemToDelete: CartItem): void {
+    this.cartItems = this.cartItems.filter((item) => item !== itemToDelete);
+  }*/
+  // Delete form MongoDB item
   deleteItem(itemToDelete: CartItem): void {
+    const url = `${this.url}/${itemToDelete}`;
+    this.http.delete(url, this.httpOptions).subscribe();
     this.cartItems = this.cartItems.filter((item) => item !== itemToDelete);
   }
 
