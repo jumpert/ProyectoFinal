390b4f5c171fd74e2b3a1dd9f38c4a575d94cb73
jumpert <juan.perezc@correo.ucu.edu.uy>
2022-11-27 22:12:47 -0300

    nuevos cambios y ticket

--- ProyectoFinal/src/app/app-routing.module.ts
+++ ProyectoFinal/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { TicketComponent } from './ticket/ticket.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
@@ -13,7 +14,7 @@ const routes: Routes = [
   { path: 'shoppingCart', component: ShoppingCartComponent },
   { path: 'home', component: ProductCardComponent },
   { path: 'users', component: UserComponent },
-  { path: 'users', component: UserComponent },
+  { path: 'shoppingCart/ticket', component: TicketComponent },
   { path: '**', component: PageNotFoundComponent}
 ]
 
